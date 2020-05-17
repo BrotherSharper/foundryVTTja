48c9900c422a8e722bdde87d3c49f72096b7048e
BrotherSharper <besardida@gmail.com>
2020-05-17 15:19:33 +0900

    update for 0.5.7, rip mystery-man

---
+++ js/main.js
@@ -0,0 +1,17 @@
+Hooks.on("preUpdateActor", (data, updatedData) =>{
+    if (data.data.token.img == "modules/foundryVTTja/img/mystery-man.svg" && updatedData.img)
+    {
+      updatedData["token.img"] = updatedData.img;
+      data.data.token.img = updatedData.img;
+    }
+  })
+  
+  
+  /**
+   * Set default values for new actors' tokens
+   */
+  Hooks.on("preCreateActor", (createData) =>{ 
+    // Set custom default token
+    if (!createData.img)
+      createData.img = "modules/foundryVTTja/img/mystery-man.svg"
+  })
