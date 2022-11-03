39eb3f72739fcf472d8d6124d81af674f5513991
BrotherSharper <besardida@gmail.com>
2022-11-03 12:40:36 +0900

    update for new manifest

--- js/main.js
+++ js/main.js
@@ -12,12 +12,12 @@ Hooks.on("preUpdateActor", (data, updatedData) =>{
   /**
    * Set default values for new actors' tokens
    */
-  Hooks.on("preCreateActor", (createData) =>{ 
+  // Hooks.on("preCreateActor", (createData) =>{ 
     // Set custom default token
-    if (!createData.img)
-      createData.img = "modules/foundryVTTja/img/mystery-man.svg"
-  })
+  //   if (!createData.img)
+  //     createData.img = "modules/foundryVTTja/img/mystery-man.svg"
+  // })
 
-  Hooks.once('libChangelogsReady', function() {
-    libChangelogs.registerConflict("foundryVTTja", "jpfoundryvtt","オンセ工房 Foundry VTT日本語コミュニティがサポートしていない本体日本語化Modが検出されました。オンセ工房が正しく機能しない恐れがありますのでいずれかを無効化してください。","critical")
-})
+//   Hooks.once('libChangelogsReady', function() {
+//     libChangelogs.registerConflict("foundryVTTja", "jpfoundryvtt","オンセ工房 Foundry VTT日本語コミュニティがサポートしていない本体日本語化Modが検出されました。オンセ工房が正しく機能しない恐れがありますのでいずれかを無効化してください。","critical")
+// })
